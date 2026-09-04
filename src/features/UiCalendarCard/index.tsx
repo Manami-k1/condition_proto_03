066ad33f6ee29cb25e8554e1a0f09f0822eefc89
Kagaya Manami <ys.traning230715@gmail.com>
2026-09-04 21:11:16 +0900

    #35: Update UiCalendarCardコンポーネントのスタイルをconst.tsに移動し、コードを整理

--- src/features/UiCalendarCard/index.tsx
+++ src/features/UiCalendarCard/index.tsx
@@ -1,10 +1,10 @@
 import type { FC } from 'react';
 
-import { COLORS } from '../../components/styles/tokens/colors';
 import type { UiCalendarCardProps } from './type';
 import { UiCard } from '../../components/ui/elements/UiCard';
 import { Box } from '@mui/material';
 import { UiTypography } from '../../components/ui/elements/UiTypography';
+import { SX_UICARD, SX_UICARD_CONTENT } from './const';
 
 export const UiCalendarCard: FC<UiCalendarCardProps> = ({
   children,
@@ -14,25 +14,10 @@ export const UiCalendarCard: FC<UiCalendarCardProps> = ({
   ...rest
 }) => {
   return (
-    <UiCard
-      {...rest}
-      dataTestId={dataTestId}
-      sx={{
-        boxShadow: `0 8px 0 ${COLORS[color][400]}`,
-        padding: '6px 9px',
-        minHeight: '156px',
-        width: '140px',
-        minWidths: '140px',
-        position: 'relative',
-      }}
-    >
+    <UiCard {...rest} dataTestId={dataTestId} sx={SX_UICARD(color)}>
       <UiTypography variant="bodySmall">{children}</UiTypography>
-
       <Box sx={{ position: 'absolute', right: '9px', bottom: '4px' }}>
-        <UiTypography
-          variant="h3Bold"
-          sx={{ marginLeft: '3px', marginBottom: 0, fontWeight: 800 }}
-        >
+        <UiTypography variant="h3Bold" sx={SX_UICARD_CONTENT}>
           {item.date}
         </UiTypography>
       </Box>
